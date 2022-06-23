23729b2fdd7c1b16a477347417a6582a697bbc90
Kilian Mauler <kilian.mauler@gmail.com>
2022-06-23 18:24:34 +0200

    [+] empty dummy methods for comms

--- src/communication.py
+++ src/communication.py
@@ -1,18 +1,39 @@
 import socket
+import fleet
 
-sock = None
-server = None
-mode = "client"
+class Packet(object):
+    def __init__(self, packetType: str, data: str):
+        self.type = packetType
+        self.data = data
+
+    def __str__(self):
+        return self.type + ";" + self.data + ";"
 
 
-def open(open_as="client", port: int = 5778, address: str = "127.0.0.1"):
-    """Opens the communication either as a server or a client"""
-    global mode, sock, server
-    mode = open_as
-    if mode == "client":
-        # sock = socket.socket
+class Connection(object):
+
+    def __init__(self, open_as="client", port: int = 5778, address: str = socket.gethostname()):
+        """Opens the communication either as a server or a client"""
+        mode = open_as
+        if mode == "client":
+            self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.connection.connect((address, port))
+        elif mode == "server":
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server.bind((address, port))
+            server.listen()
+            self.connection = server.accept()  # no while true and separate thread needed, this is a 1 to 1 connection
+        else:
+            raise ValueError("only 'server' and 'client' as arguments allowed. found: " + str(open_as))
+
+    def send_guess(self, x: int, y: int) -> bool:
         pass
-    elif mode == "server":
+
+    def await_response(self) -> fleet.FieldState:
         pass
-    else:
-        raise ValueError("only 'server' and 'client' as arguments allowed. found: " + str(open_as))
+
+    def await_guess(self) -> tuple[int, int]:
+        pass
+
+    def send_response(self, response: fleet.FieldState) -> None:
+        pass
